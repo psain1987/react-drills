49358ba9f09dc07d062b4fad3f587201c467a761
unknown <psain87@hotmail.com>
2020-10-21 17:57:39 -0500

    complete

---
+++ app-5/src/image.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Image extends Component{
+    render(){
+        return(
+            <div>
+                <img src='https://i.imgflip.com/2zfooq.jpg'/>
+            </div>
+        )
+    }
+}
+
+export default Image
