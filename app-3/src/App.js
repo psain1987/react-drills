a6a7576ce8f68564ee9303ed4031617c0bcc6d86
unknown <psain87@hotmail.com>
2020-10-21 16:35:59 -0500

    complete

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(){
+    super()
+      
+      this.state = {
+        foodString: '',
+        food: ['Bananas', 'Bacon', 'Salad', 'Chicken', 'Pasta']
+      }
+  }
+
+  handleChange(filter){
+    this.setState({foodString: filter})
+  }
+
+  render(){
+    let foodBuffet = this.state.food
+    .filter((e, i) => {
+      return e.includes(this.state.foodString)
+    })
+    .map((e, i) => {
+      return <h2 key={i} > {e}</h2>
+    })
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <input onChange={e => this.handleChange(e.target.value)} type="text" />
+    {foodBuffet}
+  </div>
   );
+  }
 }
+
 
 export default App;
